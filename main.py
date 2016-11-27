b4750f3ff7007963c5273dda1c4e1ac37b886983
Sean Behan <codebam@riseup.net>
2016-11-26 19:59:04 -0500

    it works omg!!! rofl

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-from lxml import html
+from lxml import html, etree
 from urllib.request import urlopen
-from io import StringIO, BytesIO
 
 
 def open_website(url):
@@ -16,6 +15,8 @@ def open_website(url):
 
 def parse_website(myfile):
     try:
+        # soup = BeautifulSoup(myfile)
+        # soup BeautifulSoup(myfile, "lxml")
         lxml_tree = html.fromstring(myfile)
     except:
         sys.exit("couldn't open file")
@@ -23,12 +24,17 @@ def parse_website(myfile):
     return lxml_tree
 
 
-def add_buttons(lxml_tree):
-    # http://www.w3.org/WAI/wcag1AAA.png
-    return 0
-
-def add_to_footer(lxml_tree, image):
-    lxml_tree.body.footer.append(html.Element("a"))
+def modify_html(lxml_tree):
+    images = ['http://www.w3.org/WAI/wcag1AAA.png', 'http://www.vintagecomputing.com/wp-content/images/webads/01_SNAPPLE.GIF', 'http://www.vintagecomputing.com/wp-content/images/webads/02_homepage1.gif', 'http://www.vintagecomputing.com/wp-content/images/webads/01_M0QMHHHQ.GIF', 'http://www.vintagecomputing.com/wp-content/images/webads/12_M0QMHID7.GIF', 'http://www.vintagecomputing.com/wp-content/images/webads/23_marchmadness99.gif', 'http://www.vintagecomputing.com/wp-content/images/webads/11_M0RIQ87N.GIF']
+    footer = etree.Element('footer')
+    lxml_tree.find('.//body').append(footer)
+    footer_location = lxml_tree.find('.//footer')
+    for image in images:
+        img = etree.Element('img')
+        img.set('src', image)
+        footer_location.append(img)
+    for html_tag in lxml_tree.xpath('//html'):
+        html_tag.attrib['style'] = 'background-color: blue'
     return lxml_tree
 
 
@@ -42,16 +48,36 @@ def grab_images(lxml_tree):
     return img_list
 
 
+def delete_new_elements(lxml_tree):
+    for style in lxml_tree.xpath("//style"):
+        style.getparent().remove(style)
+    for script in lxml_tree.xpath("//script"):
+        script.getparent().remove(script)
+    for link in lxml_tree.xpath("//link"):
+        link.getparent().remove(link)
+    return lxml_tree
+
+
+def delete_images(lxml_tree):
+    for img in lxml_tree.xpath("//img"):
+        img.getparent().remove(img)
+    for path in lxml_tree.xpath("//path"):
+        path.getparent().remove(path)
+    return lxml_tree
+
+
 def main():
-    meme_websites = ['https://imgflip.com/memetemplates']
+    # meme_websites = ['https://imgflip.com/memetemplates']
     # you can add more websites here grab memes from
 
     # for meme_website in meme_websites:
     #     grab_images(meme_website)
     lxml_tree = parse_website(open_website(sys.argv[1]))
 
-    lxml_tree.body.append(html.Element("footer"))
-    lxml_tree = add_to_footer(lxml_tree, '')
+    # lxml_tree.body.append(html.Element("footer"))
+    lxml_tree = delete_images(lxml_tree)
+    lxml_tree = delete_new_elements(lxml_tree)
+    lxml_tree = modify_html(lxml_tree)
     # add_buttons(lxml_tree)
 
     with open('index.html', 'wb') as f:
